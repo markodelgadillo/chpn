72b5b0a06e7934b8f4bad0a435d1e3f7505ba983
marko delgadillo <markoneseven@gmail.com>
2018-06-26 19:53:29 -0700

    #1 Added the logic that captures the value of the tip selected and adds it to the total.

--- index.js
+++ index.js
@@ -1,24 +1,33 @@
 const contributors = []
-let cost
+var cost
 
 let totalAmount = document.querySelector('#total')
-let tip = document.querySelectorAll('.tip')
+let tips = document.querySelectorAll('.tip')
 // check by converting the number into a string then splitting
 // it from the decimal point. Then save the characters after the
 // decimal point. If the length is 2, then turn off the onkeydown
 function total(totalAmount) {
-  // console.log(parseFloat(this.value).toFixed(2))
-  this.value.toString().split('.')[1].length === 2
+  this.value.toString().split('.')[1].length === 1
     ? this.setAttribute('onkeydown', 'return false')
-    : this.removeAttribute('onkeydown', '')
+    : this.removeAttribute('onkeydown')
 
   cost = parseFloat(this.value)
 }
 
 // calcluate the total amount plus tip
-function plusTip(cost) {
-  cost = cost + cost * tip.childNode.value
+function plusTip() {
+  console.log(this.value)
+  cost = cost + cost * (this.value * 0.01)
+  console.log(parseFloat(cost.toString()).toFixed(2))
 }
 
-totalAmount.addEventListener('keyup', total)
-tip.addEventListener('', plusTip)
+function keyCheck(e) {
+  if (e.keyCode === 8 && totalAmount.getAttribute('onkeydown')) {
+    totalAmount.removeAttribute('onkeydown')
+  }
+}
+
+totalAmount.addEventListener('keypress', total)
+document.addEventListener('keydown', keyCheck)
+tips.forEach(tip => tip.addEventListener('click', plusTip))
+// tip.addEventListener('', plusTip)
