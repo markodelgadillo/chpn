04660aa28e749ab145e9da8636c82888c244c1ec
marko delgadillo <markoneseven@gmail.com>
2018-06-27 19:37:33 -0700

    #1 Added logic to store the array of contributors to the localstorage of the browser.

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-const contributors = []
+// watch the Wes Bos video when he stores data in the browser cookies
+const contributors = JSON.parse(localStorage.getItem('contributors')) || []
 var cost
 
 let totalAmount = document.querySelector('#total')
 let tips = document.querySelectorAll('.tip')
-// check by converting the number into a string then splitting
-// it from the decimal point. Then save the characters after the
-// decimal point. If the length is 2, then turn off the onkeydown
+let person = document.querySelector('#person')
+let addPerson = document.querySelector('#add')
+let calculate = document.querySelector('#calculate')
+
 function total(totalAmount) {
   this.value.toString().split('.')[1].length === 1
     ? this.setAttribute('onkeydown', 'return false')
@@ -14,12 +16,13 @@ function total(totalAmount) {
   cost = parseFloat(this.value)
 }
 
-// calcluate the total amount plus tip
 function plusTip() {
   totalAmount.value.toString().split('.')[1].length === 1
     ? alert('You forgot about the cents!')
     : (cost = cost + cost * (this.value * 0.01))
-  console.log(parseFloat(cost.toString()).toFixed(2))
+  alert(
+    'With tip, the total comes out to ' + parseFloat(cost.toString()).toFixed(2)
+  )
 }
 
 function keyCheck(e) {
@@ -28,7 +31,16 @@ function keyCheck(e) {
   }
 }
 
+function addContributor() {
+  person.value === ''
+    ? alert('You need to add a name!')
+    : contributors.push(person.value)
+  person.value = ''
+  localStorage.setItem('contributors', JSON.stringify(contributors))
+  console.log(contributors)
+}
+
 totalAmount.addEventListener('keypress', total)
 totalAmount.addEventListener('keydown', keyCheck)
 tips.forEach(tip => tip.addEventListener('click', plusTip))
-// tip.addEventListener('', plusTip)
+addPerson.addEventListener('click', addContributor)
